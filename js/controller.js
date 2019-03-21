31783bac05e7ea984fa926f80ca5894536ed186d
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2019-03-21 13:44:16 +0100

    bit-show

--- js/controller.js
+++ js/controller.js
@@ -10,7 +10,6 @@ const controller = ((data, ui) => {
             // display data
             ui.displayShows(response)
         })
-
     }
 
     return {
@@ -20,3 +19,186 @@ const controller = ((data, ui) => {
 })(dataModule, uiModule)
 
 
+
+/////////////////////////////////
+
+// 1
+// const controller = (() => {
+
+//     const onResponse = (responseData) => {
+//         uiModule.displayShows(responseData);
+//     };
+
+//     const initHomepage = () => {
+//         dataModule.fetchShows(onResponse);
+//     }
+
+//     const initSingleShowPage = () => {
+//         // ...
+//     }
+
+//     return {
+//         initHomepage,
+//         initSingleShowPage
+//     }
+// })()
+
+///////////////////////////////////
+
+// 2
+// const controller = (() => {
+//     const initHomepage = () => {
+//         dataModule.fetchShows((shows) => {
+//             uiModule.displayShows(shows);
+//         });
+//     }
+
+//     const initSingleShowPage = () => {
+//         // ...
+//     }
+
+//     return {
+//         initHomepage,
+//         initSingleShowPage
+//     }
+// })()
+//////////////////////////////////////
+
+
+
+
+
+
+///////////////////////////////////////////////////////
+
+    //     function init() {
+
+    //         var tvShows;
+    
+    //         let request = $.ajax({
+    
+    //             url: 'http://api.tvmaze.com/shows',
+    //             method: 'GET'
+    
+    //         })
+    
+    //         request.done(function (msg) {
+    //             let firsFifty = msg.slice(0, 50);
+    //             tvShows = new dataModule.TvShows();
+    //             firsFifty.forEach(element => {
+    //                 let show = new dataModule.Show(element.name, element.image.medium, element.id, element.summary);
+    
+    //                 tvShows.addShow(show);
+    //             });
+    
+    //             UIModule.makeListOfMovies(tvShows.showsList)
+    //         })
+    
+    //         // search box
+    
+    //         $('#search_box').keyup(function (event) {
+    //             let value = (this).value;
+    
+    //             let request = $.ajax({
+    //                 url: `http://api.tvmaze.com/search/shows?q=${value}`,
+    //                 method: 'GET'
+    
+    //             })
+    
+    //             request.done(function (msg) {
+    //                 $("#serch_list").empty();
+    //                 tenMsg = msg.slice(0, 10);
+    //                 tenMsg.forEach(element => {
+    //                     let movieName = element.show.name;
+    //                     let id = element.show.id;
+    //                     UIModule.makeSearchList(movieName, id);
+    //                 })
+    //             })
+    
+    //         })
+    
+    //         $('#search_box').change(function (event) {
+    //             var value = $('#search_box').val();
+    //             var element = $(`*[value='${value}']`);
+    //             var id = element.attr("data-id");
+    //             window.location.href = `showInfoPage.html#${id}`
+    //         })
+    
+    //     }
+    
+    
+    
+    //     function initSinglePage() {
+    
+    //         $('#search_box').keyup(function (event) {
+    //             let value = (this).value;
+    
+    //             let request = $.ajax({
+    //                 url: `http://api.tvmaze.com/search/shows?q=${value}`,
+    //                 method: 'GET'
+    
+    //             })
+    
+    //             request.done(function (msg) {
+    //                 $("#serch_list").empty();
+    //                 tenMsg = msg.slice(0, 10);
+    //                 tenMsg.forEach(element => {
+    //                     let movieName = element.show.name;
+    //                     let id = element.show.id;
+    //                     UIModule.makeSearchList(movieName, id);
+    //                 })
+    //             })
+    
+    //         })
+    
+    //         $('#search_box').change(function (event) {
+    //             var value = $('#search_box').val();
+    //             var element = $(`*[value='${value}']`);
+    //             var id = element.attr("data-id");
+    //             window.location.href = `showInfoPage.html#${id}`
+    //             location.reload();
+    //         })
+    
+    //         let id = window.location.hash.slice(1);
+    
+    //         let request = $.ajax({
+    
+    //             url: `http://api.tvmaze.com/shows/${id}`,
+    //             method: 'GET',
+    //             data: {
+    //                 embed: ['seasons', 'cast', 'crew', 'akas']
+    //             }
+    
+    //         })
+    //         let show;
+    //         request.done(function (msg) {
+    // console.log(msg);
+    //             show = new dataModule.Show(msg.name, msg.image.original, msg.id, msg.summary);
+    
+    //             msg._embedded.seasons.forEach((element) => {
+    //                 season = new dataModule.Season(element.premiereDate, element.endDate);
+    //                 show.addSeason(season);
+    //             })
+    
+    //             msg._embedded.cast.forEach((element) => {
+    //                 cast = new dataModule.Cast(element.person.name);
+    //                 show.addCast(cast);
+    //             })
+    
+    //             msg._embedded.akas.forEach((element) => {
+    //                 AKA = new dataModule.AKA(element.country.name, element.name);
+    //                 show.addAKA(AKA);
+    //             })
+    
+    //             msg._embedded.crew.forEach((element) => {
+    //                 crew = new dataModule.Crew(element.type, element.person.name);
+    //                 show.addCrew(crew);
+    //             })
+    // console.log(show);
+    //             UIModule.makeOneMovie(show);
+    
+    //         })
+    
+
+        ///////////////////////////////////////////////////////
+    
--- js/data.js
+++ js/data.js
@@ -14,19 +14,21 @@ const dataModule = (() => {
         }
     }
 
+    
     function fetchShows(onSuccess) {
-        console.log('DATA', 'fetchShows');
+        // console.log('DATA', 'fetchShows');
 
         const showsRequestUrl = 'http://api.tvmaze.com/shows';
+        
 
         $.get(showsRequestUrl, function (showsArray) {
-            console.log("DATA", "GET request finished", showsArray);
+            // console.log("DATA", "GET request finished", showsArray);
 
             const myShows = showsArray.slice(0, 51).map((show) => {
                 const { id, name, image, rating } = show;
                 return new Show(id, name, image.medium, rating.average)
             });
-            console.log(myShows);
+            // console.log(myShows);
             onSuccess(myShows);
 
         }).fail(function () {
@@ -34,9 +36,239 @@ const dataModule = (() => {
         })
 
     }
-
     return {
         fetchShows
     }
+})()
 
-})();
+
+/////////////////////////////////////
+
+// 1
+// const dataModule = (() => {
+
+//     const fetchShows = (onSuccess) => {
+//         $.get('http://api.tvmaze.com/shows', onSuccess)
+//     }
+
+//     return {
+//         fetchShows
+//     }
+// })()
+
+/////////////////////////////////////
+
+// 2
+// const dataModule = (() => {
+
+//     const fetchShows = (onSuccess) => {
+//         $.get('http://api.tvmaze.com/shows', (responseData) => {
+//             // ...
+//             // ...
+//             onSuccess(responseData);
+//         })
+//     }
+
+//     return {
+//         fetchShows
+//     }
+// })()
+
+
+
+
+
+
+////////////////////////////////////
+
+// class Season {
+
+//     constructor(startDate, endDate) {
+//         this.startDate = startDate;
+//         this.endDate = endDate;
+//     }
+// }
+
+
+// class Cast {
+    
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// // class AKA {
+    
+// //     constructor(country, name) {
+// //         this.country = country;
+// //         this.name = name;
+// //     }
+// // }
+
+// class Crew {
+//     constructor(type, name) {
+//         this.type = type;
+//         this.name = name;
+//     }
+// }
+
+// class Show {
+
+//     constructor(name, image, id, details) {
+//         this.name = name;
+//         this.image = image;
+//         this.id = id;
+//         this.seasonsList = [];
+//         this.castsList = [];
+//         // this.AKAList = [];
+//         this.crewList = [];
+//         this.details = details
+//     }
+//     addSeason(season) {
+//         this.seasonsList.push(season);
+//     }
+//     addCast(cast) {
+//         this.castsList.push(cast);
+//     }
+//     // addAKA(AKA) {
+//     //     this.AKAList.push(AKA);
+//     // }
+//     addCrew(crew) {
+//         this.crewList.push(crew);
+//     }
+// }
+
+// class TvShows {
+
+//     constructor() {
+//         this.showsList = [];
+//         this.limit = 50;
+//     }
+//     addShow(show) {
+//         this.showsList.push(show);
+//     }
+//     getShow(id){
+        
+//         let result =  this.showsList.filter(show => show.id == id);
+//         if(result.length){
+//             return null;
+//         }
+//         return result[0];
+
+//     }
+
+// }
+
+// return {
+//     Season: Season,
+//     Cast: Cast,
+//     AKA: AKA,
+//     Crew: Crew,
+//     Show: Show,
+//     TvShows: TvShows
+// }
+
+
+        // // search box
+
+        // $('#search_box').keyup(function (event) {
+        //     let value = (this).value;
+
+        //     let request = $.ajax({
+        //         url: `http://api.tvmaze.com/search/shows?q=${value}`,
+        //         method: 'GET'
+
+        //     })
+
+        //     request.done(function (msg) {
+        //         $("#serch_list").empty();
+        //         tenMsg = msg.slice(0, 10);
+        //         tenMsg.forEach(element => {
+        //             let movieName = element.show.name;
+        //             let id = element.show.id;
+        //             UIModule.makeSearchList(movieName, id);
+        //         })
+        //     })
+
+        // })
+
+        // $('#search_box').change(function (event) {
+        //     var value = $('#search_box').val();
+        //     var element = $(`*[value='${value}']`);
+        //     var id = element.attr("data-id");
+        //     window.location.href = `showInfoPage.html#${id}`
+        // })
+
+
+        // // single page
+
+        // function initSinglePage() {
+
+        //     $('#search_box').keyup(function (event) {
+        //         let value = (this).value;
+    
+        //         let request = $.ajax({
+        //             url: `http://api.tvmaze.com/search/shows?q=${value}`,
+        //             method: 'GET'
+    
+        //         })
+    
+        //         request.done(function (msg) {
+        //             $("#serch_list").empty();
+        //             tenMsg = msg.slice(0, 10);
+        //             tenMsg.forEach(element => {
+        //                 let movieName = element.show.name;
+        //                 let id = element.show.id;
+        //                 UIModule.makeSearchList(movieName, id);
+        //             })
+        //         })
+    
+        //     })
+    
+        //     $('#search_box').change(function (event) {
+        //         var value = $('#search_box').val();
+        //         var element = $(`*[value='${value}']`);
+        //         var id = element.attr("data-id");
+        //         window.location.href = `showInfoPage.html#${id}`
+        //         location.reload();
+        //     })
+    
+        //     let id = window.location.hash.slice(1);
+    
+        //     let request = $.ajax({
+    
+        //         url: `http://api.tvmaze.com/shows/${id}`,
+        //         method: 'GET',
+        //         data: {
+        //             embed: ['seasons', 'cast', 'crew'] // , 'akas'
+        //         }
+    
+        //     })
+        //     let show;
+        //     request.done(function (msg) {
+        //     console.log(msg);
+        //         show = new dataModule.Show(msg.name, msg.image.original, msg.id, msg.summary);
+    
+        //         msg._embedded.seasons.forEach((element) => {
+        //             season = new dataModule.Season(element.premiereDate, element.endDate);
+        //             show.addSeason(season);
+        //         })
+    
+        //         msg._embedded.cast.forEach((element) => {
+        //             cast = new dataModule.Cast(element.person.name);
+        //             show.addCast(cast);
+        //         })
+    
+        //         // msg._embedded.akas.forEach((element) => {
+        //         //     AKA = new dataModule.AKA(element.country.name, element.name);
+        //         //     show.addAKA(AKA);
+        //         // })
+    
+        //         msg._embedded.crew.forEach((element) => {
+        //             crew = new dataModule.Crew(element.type, element.person.name);
+        //             show.addCrew(crew);
+        //         })
+        //         console.log(show);
+        //         UIModule.makeOneMovie(show);
+    
+        //     })
